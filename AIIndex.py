c53625505ca444b9f93dbbd8e5e2cc8d1bd6f0a5
Johan Nyholm <hansjohannyholm@gmail.com>
2015-03-14 16:41:34 +0100

    some more

--- AIIndex.py
+++ AIIndex.py
@@ -15,7 +15,7 @@ A['horn clause']=228;
 A['definite clause']=228;
 A['heuristic functions']=104;
 A['8-puzzle']=105;
-A['admissible heuristics']=106;
+A['admissible heuristics']=96;
 A['simulated annealing']=128;
 A['conjunctive normal form']=225;
 A['propositional logic']=221;
@@ -28,9 +28,12 @@ A['filtering']=581;
 A['regression']=728;
 A['linear regression']=729;
 A['fixed lag smoothing']=590;
-
-
-
+A['perceptron']=735;
+A['quantifier']=298;
+A['inference']=222;
+A['resolvent']=224;
+A['forward chaining']=229;
+A['backward chaining']=229;
 print("Index for Artificial Intelligence: A Modern Approach, Third Edition (international)");
 print("")
 for key in sorted(A):
